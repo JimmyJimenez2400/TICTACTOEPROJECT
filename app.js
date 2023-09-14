7e4fa7b998641a780267e9c0f5c8557cd889a47a
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-09-14 05:22:07 -0400

    Create getTableContainer/ call getTableContainer in renderBoard

--- app.js
+++ app.js
@@ -30,12 +30,33 @@ const GameBoard = (() => {
   return { getGameBoard };
 })();
 
-const displayController = (()=>{
+const displayController = (() => {
+  const getTableContainer = () => {
+    const boardContainer = document.querySelector('.boardContainer');
+    return boardContainer;
+  };
 
-  const renderBoard = (gameBoardArr) =>{
-    
-  }
+  const renderBoard = (gameBoardArr) => {
+    const getBoardContainer = getTableContainer();
+    const tableContainer = document.createElement('table');
+
+    let newRow;
+
+    // We want to create a 3x3 table
+    for (let i = 0; i < 3; i += 1) {
+      if (i % 3 == 0 && i != 0) {
+        //insert table row
+        newRow = tableContainer.insertRow(-1);
+      }
+    }
+
+    return tableContainer;
+  };
+
+  return { renderBoard };
 })();
+
+displayController.renderBoard();
 
 // Little global code as possible (Use factory or module)
 // If you ever need one of something, use a module
